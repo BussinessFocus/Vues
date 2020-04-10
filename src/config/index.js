03056e8016c862a0e13b00c275e26d5f588ca7e7
石老人 <1450982558@qq.com>
2020-04-10 15:04:54 +0800

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,21 @@
+
+let BaseUrl = ""; //后台访问基地址
+
+//开发环境配置
+if (process.env.NODE_ENV === "development") {
+    BaseUrl="";
+}
+//测试环境地址
+if (process.env.NODE_ENV === "test") {
+    BaseUrl="";
+}
+//正式环境地址
+if (process.env.NODE_ENV === "production") {
+    BaseUrl="";
+}
+
+console.log(process.env.NODE_ENV);
+
+export default {
+    BaseUrl
+}
